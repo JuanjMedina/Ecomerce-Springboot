80566b83c487260cef83ccf05cfe340d8f635f45
Juan Medina <jmedinaguerrero847@gmail.com>
2024-11-30 10:54:17 -0500

    Add Category entity and repository to project
    Introduce the Category entity with JPA annotations, including ID generation and name field. Implement CategoryRepository extending JpaRepository to facilitate Category data operations and provide a custom method for finding categories by name.

---
+++ src/main/java/com/juanmedina/ecomerce/model/Category.java
@@ -0,0 +1,17 @@
+package com.juanmedina.ecomerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long category_id;
+    private String name;
+}
