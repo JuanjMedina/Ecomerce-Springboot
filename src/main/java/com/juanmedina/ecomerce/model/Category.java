d291cdad29d84483a840c9376a3cb76888f687cc
Juan Medina <jmedinaguerrero847@gmail.com>
2024-12-06 16:45:11 -0500

    update name to primary key

--- src/main/java/com/juanmedina/ecomerce/model/Category.java
+++ src/main/java/com/juanmedina/ecomerce/model/Category.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long category_id;
+    private Long id;
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
