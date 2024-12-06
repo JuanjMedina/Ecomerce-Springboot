31d9cd7333d570728e25b45b1e6eb3278c2ba053
Juan Medina <jmedinaguerrero847@gmail.com>
2024-12-06 16:33:06 -0500

    Update import Product Model

--- src/main/java/com/juanmedina/ecomerce/repository/ProductsRepository.java
+++ src/main/java/com/juanmedina/ecomerce/repository/ProductsRepository.java
@@ -1,10 +1,10 @@
 package com.juanmedina.ecomerce.repository;
 
-import com.juanmedina.ecomerce.model.Products;
+import com.juanmedina.ecomerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductsRepository extends JpaRepository<Products, Long> {
+public interface ProductsRepository extends JpaRepository<Product, Long> {
 
 }
